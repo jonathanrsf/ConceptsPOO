6658f5dbb37cc58ed0598db7a763187f2a36b137
Jonathan Raùl Seminario Flores <jonathan081986@gmail.com>
2022-03-18 16:19:57 -0500

    Finish date class 1

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Date date1 = new Date(2024, 2, 29);
+                Console.WriteLine(date1);
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message);
+            }
+
+            
+        }
+    }
+}
