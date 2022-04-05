c0be8a1df1f34882a1ab5e503a7a8302ed28169c
JonathanRSF <jonathan018986@gmail.com>
2022-04-05 18:03:05 -0500

    Finish POO Concepts

--- ConceptsPOO/ConceptsPOO/HourlyEmployee.cs
+++ ConceptsPOO/ConceptsPOO/HourlyEmployee.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace ConceptsPOO
 {
-    public class CommissionEmployee : Employee
+    public class HourlyEmployee : Employee
     {
-        public float CommissionPercentaje { get; set; }
-        public decimal Sales { get; set; }
-
+        public float Hours { get; set; }
+        public decimal HourValue { get; set; }
         public override decimal GetValueToPay()
         {
-            return Sales * (decimal)CommissionPercentaje;
+            return (decimal)Hours * HourValue;
         }
 
         public override string ToString()
         {
             return $"{base.ToString()} \n" +
-                $"\tCommision percentaje : {$"{CommissionPercentaje:P2}",20} \n" +
-                $"\tSales................: {$"{Sales:C2}",20} \n" +
+                $"\tHours................: {$"{Hours:N2}",20} \n" +
+                $"\tHours Value..........: {$"{HourValue:C2}",20} \n" +
                 $"\tValue to pay.........: {$"{GetValueToPay():C2}",20}";
         }
+
     }
 }
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConceptsPOO
 {
@@ -8,7 +9,7 @@ namespace ConceptsPOO
         {
             Employee employee1 = new SalaryEmployee()
             {
-                Id = 1010,
+                Id = 1011,
                 FirstName = "Jr",
                 LastName = "Sf",
                 BirthDate = new Date(1990, 2, 8),
@@ -18,18 +19,71 @@ namespace ConceptsPOO
             };
             Employee employee2 = new CommissionEmployee()
             {
-                Id = 1011,
+                Id = 1012,
                 FirstName = "La",
                 LastName = "Oc",
                 BirthDate = new Date(1992, 2, 8),
                 HiringDate = new Date(2021, 5, 9),
                 IsActive = true,
-                CommissionPercentaje = 0.03f,
+                CommissionPercentaje = 0.037f,
                 Sales = 122000M                
             };
+            Employee employee3 = new HourlyEmployee()
+            {
+                Id = 1013,
+                FirstName = "Churri@",
+                LastName = "Bonit@",
+                BirthDate = new Date(2022, 08, 12),
+                HiringDate = new Date(2023, 08, 15),
+                IsActive = true,
+                Hours = 120,
+                HourValue = 12
+            };
+            Employee employee4 = new BaseCommissionEmployee()
+            {
+                Id = 1014,
+                FirstName = "La1",
+                LastName = "Oc1",
+                BirthDate = new Date(1992, 2, 8),
+                HiringDate = new Date(2021, 5, 9),
+                IsActive = true,                
+                Base = 9000M,
+                CommissionPercentaje = 0.015f,
+                Sales = 102000M,
+            };
 
-            Console.WriteLine(employee1);
-            Console.WriteLine(employee2);
+            ICollection<Employee> employees = new List<Employee>()
+            { employee1, employee2, employee3, employee4 };
+
+            decimal payroll = 0;
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee);
+                payroll += employee.GetValueToPay();
+            }
+            Console.WriteLine("                                ===================");
+            Console.WriteLine($"TOTAL                          {$"{payroll:C2}", 20}");
+
+            //Console.WriteLine(employee1);
+            //Console.WriteLine(employee2);
+            //Console.WriteLine(employee3);
+            //Console.WriteLine(employee4);
+            Invoice invoice1 = new Invoice()
+            {
+                Id = 1,
+                Description = "Xiaomi Mi9",
+                Price = 2230M,
+                Quantity = 5
+            };
+            Invoice invoice2 = new Invoice()
+            {
+                Id = 2,
+                Description = "Chuleton",
+                Price = 130M,
+                Quantity = 4.7f
+            };
+            Console.WriteLine(invoice1);
+            Console.WriteLine(invoice2);
 
 
         }
